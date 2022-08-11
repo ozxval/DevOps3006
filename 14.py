6d49f059199a508e66766e01a0b18f61770f959d
Buzy <38189339+ozxval@users.noreply.github.com>
2022-08-11 20:16:24 +0300

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,5 +1,5 @@
 import ast
 my_file = open("config.json")
-c = list(ast.literal_eval(my_file.read()))
+c = list(ast.literal_eval(my_file.read())) (hallo)
 for i in range(len(c)):
     print(c[i]["name"])
